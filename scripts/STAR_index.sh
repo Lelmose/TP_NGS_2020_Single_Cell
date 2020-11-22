bea5eade5ae813fd5d3484ea96ec486e1e414a1c
Bastien Mollet <bastien.omllet@ens-lyon.fr>
2020-11-22 14:52:47 +0000

    Updates on STAR related scripts

--- scripts/STAR_index.sh
+++ scripts/STAR_index.sh
@@ -2,15 +2,17 @@
 
 data="/home/rstudio/data/mydatalocal"
 mkdir -p $data
-cd STARindex
-
+cd $data
+mkdir -p STAR_index
+cd STAR_index
+ls
 # The runMode 'genomeGenerate' generate a STAR compatible genome file that we will 
 # need to perform alignments
 # --sjbdOverhang indicate what is the maximum possible stretch of sequence that can
 # be found on one side of a spicing site. As our fragments are at most 43 we set 
 # this value to 42.
-STAR --runMode genomeGenerate --genomeDir Indexes\
---runThreadN 7\
---genomeFastaFiles Mus_musculus.GRCm38.dna.primary_assembly.fa\
---sjbdGTFfile Mus_musculus.GRCm38.101.gtf\
---sjbdOverhang 42
+STAR --runMode genomeGenerate --genomeDir Indexes \
+--runThreadN 7 \
+--genomeFastaFiles Mus_musculus.GRCm38.dna.primary_assembly.fa \
+--sjdbGTFfile Mus_musculus.GRCm38.101.gtf \
+--sjdbOverhang 42
--- scripts/run_STAR.sh
+++ scripts/run_STAR.sh
@@ -12,7 +12,7 @@ STAR=`ls /home/rstudio/data/mydatalocal/trimmomatic_data`
 
 # As we treat a large amount of files (unusual) we will keep the index in memory to save time.
 # We do so through STAR --genomeLoad
-refdir=/ifb/data/mydatalocal/STAR_index/Indexes
+refdir=/ifb/data/mydatalocal/STAR_index/Indexes/
 STAR --genomeLoad LoadAndExit  --genomeDir $refdir
 
 # For each element in our liqt run STAR GeneCounts
@@ -25,11 +25,11 @@ targetdir=/ifb/data/mydatalocal/STAR_data/$star
 # zcat correspond to compressed files.
 # --outSAMtype can be used to sort the counts in the same time they're generated. 
 # In our cases there is too much files to do so. (We wiil do it later using samtoolsort.sh)
-
-STAR --genomeDir $genomdir \
+STAR --genomeLoad LoadAndKeep  --genomeDir $refdir
+STAR --genomeDir $refdir \
       --readFilesIn $startdir \
       --readFilesCommand zcat \
-      --outSAMtype BAM unsorted \
+      --outSAMtype BAM Unsorted \
       --quantMode GeneCounts \
       --outFileNamePrefix $targetdir
 done
