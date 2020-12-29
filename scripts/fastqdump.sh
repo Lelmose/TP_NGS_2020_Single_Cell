4f53041e0726dbda003609c6fdb6e99182b22fe0
Bastien <bastie.mollet@ens-lyon.fr>
2020-12-29 18:05:45 +0000

    Commented version of fastqdump.sh

--- scripts/fastqdump.sh
+++ scripts/fastqdump.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
 data="/ifb/data/mydatalocal"
+
+# Create data and sra_data directories if not existing
 mkdir -p $data
 cd $data
+
 mkdir -p sra_data
 cd sra_data
 
+# Create each line of SRR corresponds to an accession number to get on NCBI
 SRR=`cat /ifb/data/mydatalocal/SRR_Acc_List.txt`
 
 # For each element in our SRR file use the accession number to download the corresponding data
