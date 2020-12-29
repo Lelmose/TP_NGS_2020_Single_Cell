3be5a299c7c6fb092eb896b684efc7e918ab893f
Bastien <bastie.mollet@ens-lyon.fr>
2020-12-29 18:25:45 +0000

    Commented version of fastqc.sh

--- scripts/fastqc.sh
+++ scripts/fastqc.sh
@@ -4,17 +4,16 @@ data="/ifb/data/mydatalocal"
 mkdir -p $data
 cd $data
 
-# Create a new directory for fastqc_data
+# Create a new directory for fastqc_data if not existing
 mkdir -p fastqc_data_trimed
 
-
-# Create a new directory for fastqc_data
+# Selection randomly 10 cells in the dataset to run fastqc on them
 FASTQC=`find /ifb/data/mydatalocal/trimmomatic_data/. -type f | shuf -n 10`
 
-# For each element in our SRR file use the accession number to download the corresponding data
+# For each cell produce a fastqc report of quality
 for fastqc in $FASTQC
 do
+
 fastqc $fastqc -o ./fastqc_data_trimed 
-echo $fastqc
-# fastq-dump $srr --gzip
+
 done
