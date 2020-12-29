dac9e80efda8dba4c3ccef9c14b63368d88daed2
Bastien <bastie.mollet@ens-lyon.fr>
2020-12-29 18:48:20 +0000

    Commented version of salmon index

--- scripts/salmon_index.sh
+++ scripts/salmon_index.sh
@@ -2,9 +2,11 @@
 
 data="/home/rstudio/data/mydatalocal"
 mkdir -p $data
-
 cd $data
 
 mkdir -p Ref_seq
 cd Ref_seq
+
+# Salmon index is build from the reference sequence bank: Mus_musculus.GRCm38.cdna.all.fa 
+# and the annotations: mouse_index_unzipped
 salmon index -t Mus_musculus.GRCm38.cdna.all.fa -i mouse_index_unzipped  -k 31 
